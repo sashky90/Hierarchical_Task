e641bca6eeaebe776d5df6220905c2214ba640f9
sashky90 <sashky@ymail.com>
2016-02-03 17:26:34 +0200

    Hierarchical task complete

---
+++ Hierarchical_Task/Person.cs
@@ -0,0 +1,58 @@
+﻿namespace Hierarchical_Task
+{
+    using System;
+
+    public class Person
+    {
+        private const int MinimumEmployeesPerBoss = 2;
+
+        public Person(string name, Person boss = null)
+        {
+            this.Name = name;
+            this.Boss = boss;
+            this.Employees = new Person[MinimumEmployeesPerBoss];
+        }
+
+        public string Name { get; private set; }
+
+        public Person Boss { get; private set; }
+
+        public Person[] Employees { get; private set; }
+
+        public void AddEmployee(Person employee)
+        {
+            if (this.Employees[MinimumEmployeesPerBoss - 1] == null)
+            {
+                for (int i = 0; i < this.Employees.Length; i++)
+                {
+                    if (this.Employees[i] == null)
+                    {
+                        this.Employees[i] = employee;
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                throw new System.ArgumentOutOfRangeException("Cannot add more than " + MinimumEmployeesPerBoss + " employees per person");
+            }
+        }
+
+        public Person GetBoss()
+        {
+            return this.Boss;
+        }
+
+        public bool HasBoss()
+        {
+            if (this.Boss != null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Hierarchical_Task/Program.cs
@@ -0,0 +1,87 @@
+﻿namespace Hierarchical_Task
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            const string ExitMessage = "exit";
+
+            List<Person> personList = new List<Person>();
+            string employeeInput1 = Console.ReadLine().Trim();
+            string employeeInput2 = Console.ReadLine().Trim();
+            string hierarchy;
+
+            while ((hierarchy = Console.ReadLine().Trim()) != ExitMessage)
+            {
+                if (string.IsNullOrEmpty(hierarchy) || !hierarchy.Contains(" - "))
+                {
+                    Console.WriteLine("Wrong Input. Try again!");
+                    continue;
+                }
+
+                string[] hierarchyArray = hierarchy.Split(new string[] { " - " }, StringSplitOptions.None);
+                Person manager;
+                if (personList.Count == 0)
+                {
+                    manager = new Person(hierarchyArray[0]);
+                    personList.Add(manager);
+                }
+                else
+                {
+                    manager = personList.Find(employee => employee.Name == hierarchyArray[0]);
+                }
+
+                Person emp = new Person(hierarchyArray[1], manager);
+                try
+                {
+                    manager.AddEmployee(emp);
+                }
+                catch (ArgumentOutOfRangeException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                
+                personList.Add(emp);
+            }
+
+            var employeePerson1 = personList.Find(employee => employee.Name == employeeInput1);
+            var employeePerson2 = personList.Find(employee => employee.Name == employeeInput2);
+            var listBossesPerson1 = GetBossList(employeePerson1);
+            var listBossesPerson2 = GetBossList(employeePerson2);
+
+            var commonBoss = GetCommonBoss(listBossesPerson1, listBossesPerson2).Name;
+            Console.WriteLine(commonBoss);
+        }
+
+        public static List<Person> GetBossList(Person person)
+        {
+            var bossList = new List<Person>();
+
+            while (person.HasBoss())
+            {
+                var boss = person.GetBoss();
+                bossList.Add(boss);
+                person = boss;
+            }
+
+            return bossList;
+        }
+
+        public static Person GetCommonBoss(List<Person> list1, List<Person> list2)
+        {
+            for (int i = 0; i < list1.Count; i++)
+            {
+                var index = list2.IndexOf(list1[i]);
+                if (index != -1)
+                {
+                    return list1[i];
+                }
+            }
+
+            throw new ArgumentNullException();
+        }
+    }
+}
